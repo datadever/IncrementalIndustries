fe102bdc73a63687579abb297c7eb4fc5afd2b33
Sean <datadever@gmail.com>
2022-07-28 23:50:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Incremental Industries
+
+Incremental Industries is a Quest based Tech Modpack developed for 1.18.2
+
+NOTE: This pack has been developed using the Fabric Modloader
+
+What to expect in this pack:
+- Deep Integration Between Modern Industrialization & Create leading to hundreds of modified recipes across both mods to give you a seamless progression experience.
+- A detailed quest book spanning chapters focusing on each age, starting with primitive Create machines, through to electricity, chemistry, nuclear and beyond!
+- Fun quality of life mods like Kibe, Wired Redstone and more to reduce the early game grind while giving you full control over your factory and assembly lines.
+- Generous quest rewards customized toward what you're currently working on in the pack.
+- AE2 unlocks early-mid game to let you scale up your factory to tackle the more challenging mid-end game recipes.
+- Cosmetic mods such as Chisel & Factory Block are included for all your creative needs.
+- End game recipes are designed to keep you engaged and explore the mods, rather than having you go AFK for hours.
+- Designed from the ground up to be played in Peaceful mode, although not required.
+
+Please use the issues tab here for technical support and feedback
---
+++ kubejs/startup_scripts/script.js
@@ -0,0 +1,44 @@
+// priority: 0
+
+console.info('Hello, World! (You will only see this line once in console, during startup)')
+
+function incomplete_circuit(event, circuit_prefix) {
+	event.create(`incomplete_${circuit_prefix}_circuit`)
+}
+
+onEvent('item.registry', event => {
+	// Register new items here
+	// event.create('example_item').displayName('Example Item')
+
+	event.create("brass_dust")
+	event.create("zinc_dust")
+
+	incomplete_circuit(event, "analog")
+	incomplete_circuit(event, "electronic")
+	incomplete_circuit(event, "digital")
+	incomplete_circuit(event, "quantum")
+
+	event.create("incomplete_processing_unit_board")
+	event.create("incomplete_blank_pattern")
+
+	event.create("mob_slaughtering_drill_head")
+	event.create("mob_slaughtering_drill")
+
+	event.create("andesite_alloy_drill_head")
+	event.create("andesite_alloy_drill")
+
+	event.create("dirty_andesite_alloy_drill")
+	event.create("dirty_mob_slaughtering_drill")
+	event.create("dirty_copper_drill")
+	event.create("dirty_bronze_drill")
+	event.create("dirty_steel_drill")
+	event.create("dirty_stainless_steel_drill")
+	event.create("dirty_gold_drill")
+	event.create("dirty_aluminum_drill")
+	event.create("dirty_titanium_drill")
+})
+
+onEvent('block.registry', event => {
+	// Register new blocks here
+	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+})
